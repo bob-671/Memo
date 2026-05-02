eaeb5842cf972b556691ab928dffa822a29fa5bb
bob-671 <borhane_abdelmalek.taik@univ-constantine2.dz>
2026-05-02 11:29:04 +0100

    update dashboard UI and fix sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import User from './dashboards/User';
+import AiTracker from './dashboards/AiTracker';
+import MealPlan from './dashboards/MealPlan';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
           <Route path="/User" element={<User />} />
+          <Route path="/AiTracker" element={<AiTracker />} />
+          <Route path="/MealPlan" element={<MealPlan />} />
       </Routes>
     </BrowserRouter>
   );
